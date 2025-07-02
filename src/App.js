bb72ac3fadbeea8f746907c1b5f9ce6412239bad
WaldoSilva2528 <valdo252831@gmail.com>
2025-07-02 14:55:45 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Painel Administrativo</h1>
+      <p>Sistema completo funcionando.</p>
+    </div>
+  );
+}
+export default App;
